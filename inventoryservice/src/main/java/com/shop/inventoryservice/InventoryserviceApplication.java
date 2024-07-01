cab5da0044cb3fc75f55d03a0dfb4d36a3d964cd
Halieeim <ahmed.haliem14@gmail.com>
2024-07-01 15:05:30 +0300

    Discovery Server

--- inventoryservice/src/main/java/com/shop/inventoryservice/InventoryserviceApplication.java
+++ inventoryservice/src/main/java/com/shop/inventoryservice/InventoryserviceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class
-InventoryserviceApplication {
+//@EnableEurekaClient
+public class InventoryserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryserviceApplication.class, args);
--- orderservice/src/main/java/com/shop/orderservice/service/OrderService.java
+++ orderservice/src/main/java/com/shop/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -30,8 +30,8 @@ public class OrderService {
         List<String> skuCodesList = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
         // Call inventory service and check if the product is in the stock before placing the order
         // Synchronous Communication
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventoryservice/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodesList).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
