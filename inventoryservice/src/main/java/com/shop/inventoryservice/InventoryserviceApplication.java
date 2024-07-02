3fd216aec678153f3bd51f0192524744e3f31b15
Halieeim <ahmed.haliem14@gmail.com>
2024-07-02 13:56:09 +0300

    API Gateway

--- inventoryservice/src/main/java/com/shop/inventoryservice/InventoryserviceApplication.java
+++ inventoryservice/src/main/java/com/shop/inventoryservice/InventoryserviceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnableEurekaClient
 public class InventoryserviceApplication {
 
 	public static void main(String[] args) {
